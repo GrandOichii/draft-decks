8a478e06e7a5ee16f99fcb49056e35c06036898f
Igor Yastrebov <i.hawk@yandex.ru>
2025-10-06 15:01:21 +0300

    Initial commit

---
+++ suggest.py
@@ -0,0 +1,21 @@
+from os import listdir
+from os.path import splitext
+import random
+
+IN = 'sets.txt'
+
+sets = open(IN, 'r').read().split('\n')
+drafted = [splitext(p)[0] for p in listdir('./') if splitext(p)[0] in sets]
+
+for s in sets:
+    mark = ' '
+    if s in drafted:
+        mark = 'X'
+    print(f'[{mark}] {s}')
+
+not_drafted = [s for s in sets if s not in drafted]
+if len(not_drafted) == 0:
+    print('\nAll sets drafted! Congrats!')
+    exit(0)
+
+print(f'\nSuggestion for next set to draft: {not_drafted[random.randint(0, len(not_drafted))]}')
